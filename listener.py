38d1e238bb704651151aca7407b65c2fbf803e7e
Atsuya Morita <amazingpower1315@gmail.com>
2023-11-29 17:38:50 +0900

    Add a Command

--- listener.py
+++ listener.py
@@ -3,8 +3,8 @@ from rclpy.node import Node
 from person_msgs.msg import Person
 
 def cb(msg):
-     global node
-     node.get_logger().info("Listen: %s" % msg)
+    global node
+    node.get_logger().info("Listen: %s" % msg) 
 
 rclpy.init()
 node = Node("listener")
--- talker.py
+++ talker.py
@@ -1,18 +1,17 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Person
+from person_msgs.msg import Person
 
 rclpy.init()
 node = Node("talker")
 pub = node.create_publisher(Person, "person", 10)
 n = 0
-def cd():
-    global n
-    msg = Person()
-    msg.name = ”森田敦也”
-    msg.age = n
-    pub.publish(msg)
-    n += 1
-
-node.create_timer(0.5, cd)
+def cb():
+        global n
+        msg = Person()
+        msg.name = "name"       
+        msg.age = "age"
+        pub.publish(msg)
+        n += 1
+node.create_timer(0.5, cb)
 rclpy.spin(node)
